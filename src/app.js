8211a39aaffd8b2a88fa6e21b59c8fda5627b97c
hapyvirus <serenity2011@naver.com>
2025-03-03 11:11:29 +0900

    created Upload API, GET

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import * as dotenv from "dotenv";
 import express from "express";
 import cors from "cors";
+import multer from "multer";
 import productRoute from "./routes/products.js"
 import articleRoute from "./routes/articles.js";
 import commentRoute from "./routes/comments.js";
@@ -18,6 +19,12 @@ app.use("/products", productRoute);
 app.use("/articles", articleRoute);
 app.use("/comments", commentRoute);
 
+const upload = multer({ dest: 'upload/'});
+
+app.post('/files', upload.single('attachment'), (req, res) => {
+  const path = `/files/${req.file.filename}`
+  res.json({ path });
+})
 
 app.listen(3000, () => {
   console.log("Server is listening on PORT ");
