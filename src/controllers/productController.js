c60b62db56dfe3e38cfe4e51d6e4b0eb90e8029f
hapyvirus <serenity2011@naver.com>
2025-03-20 19:06:23 +0900

    refactor: Product Controller/Route 분리

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -1,87 +1,68 @@
-import express from "express";
 import prisma from "../config/prisma.js";
 import { assert } from "superstruct";
 import asyncHandler from "../middleWares/errorHandler.js";
 import { CreateProduct, PatchProduct } from "../middleWares/structs.js";
 
+export const getProuct = asyncHandler(async (req, res) => {
+  const { offset = 0, limit = 10, order = "newest", search = "" } = req.query;
+  let orderBy;
+  switch (order) {
+    case "oldest":
+      orderBy = { createdAt: "asc" };
+      break;
+    case "newest":
+    default:
+      orderBy = { createdAt: "desc" };
+  }
+  const products = await prisma.product.findMany({
+    where: {
+      OR: [{ name: { contains: search, mode: "insensitive" } }],
+    },
+    select: {
+      id: true,
+      name: true,
+      price: true,
+      createdAt: true,
+    },
+    orderBy,
+    skip: parseInt(offset),
+    take: parseInt(limit),
+  });
+  res.status(200).send(products);
+});
+
+export const CreateProduct = asyncHandler(async (req, res) => {
+  assert(req.body, CreateProduct);
+  const product = await prisma.product.create({
+    data: req.body,
+  });
+  res.status(201).send(product);
+});
+
+export const getProductDetail = asyncHandler(async (req, res) => {
+  const { id } = req.params;
+  const products = await prisma.product.findUnique({
+    where: { id },
+  });
+  res.status(200).send(products);
+});
+
+export const patchProduct = asyncHandler(async (req, res) => {
+  assert(req.body, PatchProduct);
+  const { id } = req.params;
+  const products = await prisma.product.update({
+    where: { id },
+    data: req.body,
+  });
+  res.status(201).send(products);
+});
+
+export const deleteProduct = asyncHandler(async (req, res) => {
+  const { id } = req.params;
+  await prisma.product.delete({
+    where: { id },
+  });
+  res.status(204);
+});
 
 
-productRoute
-  .route("/")
-  .get(
-    asyncHandler(async (req, res) => {
-      const {
-        offset = 0,
-        limit = 10,
-        order = "newest",
-        search = "",
-      } = req.query;
-      let orderBy;
-      switch (order) {
-        case "oldest":
-          orderBy = { createdAt: "asc" };
-          break;
-        case "newest":
-        default:
-          orderBy = { createdAt: "desc" };
-      }
-      const products = await prisma.product.findMany({
-        where: {
-          OR: [{ name: { contains: search, mode: "insensitive" } }],
-        },
-        select: {
-          id: true,
-          name: true,
-          price: true,
-          createdAt: true,
-        },
-        orderBy,
-        skip: parseInt(offset),
-        take: parseInt(limit),
-      });
-      res.status(200).send(products);
-    })
-  )
-  .post(
-    asyncHandler(async (req, res) => {
-      assert(req.body, CreateProduct);
-      const product = await prisma.product.create({
-        data: req.body,
-      });
-      res.status(201).send(product);
-    })
-  );
-
-productRoute
-  .route("/:id")
-  .get(
-    asyncHandler(async (req, res) => {
-      const { id } = req.params;
-      const products = await prisma.product.findUnique({
-        where: { id },
-      });
-      res.status(200).send(products);
-    })
-  )
-  .patch(
-    asyncHandler(async (req, res) => {
-      assert(req.body, PatchProduct);
-      const { id } = req.params;
-      const products = await prisma.product.update({
-        where: { id },
-        data: req.body,
-      });
-      res.status(201).send(products);
-    })
-  )
-  .delete(
-    asyncHandler(async (req, res) => {
-      const { id } = req.params;
-      await prisma.product.delete({
-        where: { id },
-      });
-      res.status(204);
-    })
-  );
-
-export default productRoute;
--- src/routes/productRoute.js
+++ src/routes/productRoute.js
@@ -1,3 +1,18 @@
 import express from "express";
+import {
+  CreateProduct,
+  deleteProduct,
+  getProductDetail,
+  getProuct,
+  patchProduct,
+} from "../controllers/productController";
 
 const productRoute = express.Router();
+
+productRoute.get("/", getProuct);
+productRoute.post("/", CreateProduct);
+productRoute.get("/:id", getProductDetail);
+productRoute.patch("/:id", patchProduct);
+productRoute.delete("/:id", deleteProduct);
+
+export default productRoute;
