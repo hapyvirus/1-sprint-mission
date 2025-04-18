4f0613c013e4d9c3de655ac8f4db945c83406873
hapyvirus <serenity2011@naver.com>
2025-04-19 01:42:07 +0900

    feat: skima 수정, query 작성

--- sprint_7/queries.sql
+++ sprint_7/queries.sql
@@ -8,7 +8,9 @@
   - 닉네임을 "test"로 업데이트
   - 현재 로그인한 유저 id가 1이라고 가정
 */
-
+UPDATE users 
+SET nickname='test' 
+WHERE id = 1;
 
 /*
   2. 내가 생성한 상품 조회
@@ -16,12 +18,20 @@
   - 최신 순으로 정렬
   - 10개씩 페이지네이션, 3번째 페이지
 */
-
+SELECT *
+FROM Products
+WHERE products.user_id = 1
+ORDER BY createdAt DESC
+LIMIT 10
+OFFSET 20;
 
 /*
   3. 내가 생성한 상품의 총 개수
 */
 
+SELECT COUNT(*)
+FROM Products
+WHERE products.user_id = 1;
 
 /*
   4. 내가 좋아요 누른 상품 조회
@@ -30,16 +40,30 @@
   - 10개씩 페이지네이션, 3번째 페이지
 */
 
+SELECT *
+FROM productlikes 
+JOIN products ON productlikes.product_id = products.id
+WHERE productlikes.user_id = 1
+ORDER BY products.createdAt DESC
+LIMIT 10
+OFFSET 20;
 
 /*
   5. 내가 좋아요 누른 상품의 총 개수
 */
+
+SELECT COUNT(*)
+FROM productlikes
+WHERE productlikes.user_id = 1;
 
 
 /*
   6. 상품 생성
   - 현재 로그인한 유저 id가 1이라고 가정
 */
+
+INSERT INTO products (user_id,images,title ,content ,price)
+VALUES (1, ARRAY['https://example.com/images/alice1.png'], 'hub', '맥북용 hub 입니다.', 5000);
 
 
 /*
@@ -49,12 +73,23 @@
   - 10개씩 페이지네이션, 1번째 페이지
   - 각 상품의 좋아요 개수를 포함해서 조회하기
 */
+SELECT products.*, COUNT(productlikes.like_id) AS likeCount
+FROM products
+LEFT JOIN productlikes ON products.id = productlikes.product_id
+WHERE products.title = 'test'
+GROUP BY products.id, products.user_id, products.title, products.content, products.price, products.images, products.createdAt, products.updatedAt
+ORDER BY products.createdAt DESC
+LIMIT 10;
+
 
 
 /*
   8. 상품 상세 조회
   - 1번 상품 조회
 */
+SELECT *
+FROM products
+WHERE id = 1;
 
 
 /*
@@ -62,29 +97,44 @@
   - 1번 상품 수정
 */
 
+UPDATE products
+SET title = '짝퉁'
+WHERE id = 1;
+
 
 /*
   10. 상품 삭제
   - 1번 상품 삭제
 */
 
+DELETE FROM products
+WHERE id = 1;
 
 /*
   11. 상품 좋아요
   - 1번 유저가 2번 상품 좋아요
 */
 
+WITH new_like AS (INSERT INTO likes DEFAULT VALUES RETURNING id)
+
+INSERT INTO productlikes (product_id, like_id, user_id) 
+SELECT 2, id, 1 FROM new_like;
 
 /*
   12. 상품 좋아요 취소
   - 1번 유저가 2번 상품 좋아요 취소
 */
 
+DELETE FROM productlikes
+WHERE product_id = 2 AND user_id = 1
 
 /*
   13. 상품 댓글 작성
   - 1번 유저가 2번 상품에 댓글 작성
 */
+
+INSERT INTO comments (user_id, product_id, content) 
+VALUES ('1', 'product_2', '이 제품 정말 강추 입니다 !');
 
 
 /*
@@ -94,4 +144,9 @@
   - 댓글 날짜 2025-03-25 기준으로 커서 페이지네이션
   - 10개씩 페이지네이션
 */
+SELECT *
+FROM comments 
+WHERE product_id = 1 AND createdAt < '2025-03-25'
+ORDER BY createdAt DESC
+LIMIT 10;
 
--- sprint_7/schema.sql
+++ sprint_7/schema.sql
@@ -1,19 +1,19 @@
 BEGIN;
 
 CREATE TABLE users (
-    id SERIAL PRIMARY KEY,
-    email text NOT NULL UNIQUE,
-    nickname text NOT NULL ,
-    password text NOT NULL ,
-    image text[],
-    provider text DEFAULT local,
-    providerId text,
-    createdAt TIMESTAMPTZ DEFAULT NOW(),
-    updatedAt TIMESTAMPTZ DEFAULT NOW(),
-  )
+  id SERIAL PRIMARY KEY,
+  email text NOT NULL UNIQUE,
+  nickname text NOT NULL ,
+  password text NOT NULL ,
+  image text[],
+  provider text DEFAULT 'local',
+  providerId text,
+  createdAt TIMESTAMPTZ DEFAULT NOW(),
+  updatedAt TIMESTAMPTZ DEFAULT NOW()
+);
 
 CREATE TABLE articles (
-  id VARCHAR(50) PRIMARY KEY,
+  id SERIAL PRIMARY KEY,
   user_id INTEGER NOT NULL,
   title text NOT NULL,
   content text NOT NULL,
@@ -23,11 +23,11 @@ CREATE TABLE articles (
   updatedAt TIMESTAMPTZ DEFAULT NOW(),
   CONSTRAINT title_length CHECK (LENGTH(title) <= 10),
   CONSTRAINT content_length CHECK (LENGTH(content) > 10),
-  FOREIGN KEY (user_id) REFERENCES users (id),
-)
+  FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE SET NULL
+);
 
 CREATE TABLE products (
-  id VARCHAR(50) PRIMARY KEY,
+  id SERIAL PRIMARY KEY,
   user_id INTEGER NOT NULL,
   images text[] NOT NULL,
   title text NOT NULL,
@@ -35,81 +35,83 @@ CREATE TABLE products (
   price INTEGER NOT NULL,
   likecount INTEGER DEFAULT 0,
   createdAt TIMESTAMPTZ DEFAULT NOW(),
-  updatedAt TIMESTAMPTZ DEFAULT NOW(), 
-  FOREIGN KEY (user_id) REFERENCES users (id),
+  updatedAt TIMESTAMPTZ DEFAULT NOW(),
+  FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE SET NULL,
   CONSTRAINT title_length CHECK (LENGTH(title) <= 10),
-  CONSTRAINT content_length CHECK (LENGTH(content) > 10),
-)
+  CONSTRAINT content_length CHECK (LENGTH(content) > 10)
+);
 
 CREATE TABLE comments (
-  id VARCHAR(50) PRIMARY KEY,
+  id SERIAL PRIMARY KEY,
   user_id INTEGER NOT NULL,
-  product_id VARCHAR(50),
-  article_id VARCHAR(50),
+  product_id INTEGER,
+  article_id INTEGER,
   content text NOT NULL,
   createdAt TIMESTAMPTZ DEFAULT NOW(),
   updatedAt TIMESTAMPTZ DEFAULT NOW(),
-  FOREIGN KEY (user_id) REFERENCES users (id),
-  FOREIGN KEY (product_id) REFERENCES products (id),
-  FOREIGN KEY (article_id) REFERENCES articles (id),
-  CONSTRAINT content_length CHECK (LENGTH(content) > 5),
-)
+  FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE SET NULL,
+  FOREIGN KEY (product_id) REFERENCES products (id) ON DELETE CASCADE,
+  FOREIGN KEY (article_id) REFERENCES articles (id) ON DELETE CASCADE,
+  CONSTRAINT content_length CHECK (LENGTH(content) > 5)
+);
 
 CREATE TABLE likes (
-  id VARCHAR(50) PRIMARY KEY,
+  id SERIAL PRIMARY KEY,
   createdAt TIMESTAMPTZ DEFAULT NOW(),
-  updatedAt TIMESTAMPTZ DEFAULT NOW(),
-)
+  updatedAt TIMESTAMPTZ DEFAULT NOW()
+);
 
 CREATE TABLE articlelikes (
-  article_id VARCHAR(50) NOT NULL,
-  like_id VARCHAR(50) NOT NULL,
+  article_id INTEGER NOT NULL,
+  like_id INTEGER NOT NULL,
   user_id INTEGER NOT NULL,
   PRIMARY KEY (article_id, like_id, user_id),
   FOREIGN KEY (article_id) REFERENCES articles (id),
-  FOREIGN KEY (like_id) REFERENCES likes (id),
-  FOREIGN KEY (user_id) REFERENCES users (id),
-)
+  FOREIGN KEY (like_id) REFERENCES likes (id) ON DELETE CASCADE,
+  FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
+);
 
 CREATE TABLE productlikes (
-  product_id VARCHAR(50) NOT NULL,
-  like_id VARCHAR(50) NOT NULL,
+  product_id INTEGER NOT NULL,
+  like_id INTEGER NOT NULL,
   user_id INTEGER NOT NULL,
   PRIMARY KEY (product_id, like_id, user_id),
-  FOREIGN KEY (product_id) REFERENCES products (id),
-  FOREIGN KEY (like_id) REFERENCES likes (id),
-  FOREIGN KEY (user_id) REFERENCES users (id),
-)
+  FOREIGN KEY (product_id) REFERENCES products (id) ON DELETE CASCADE,
+  FOREIGN KEY (like_id) REFERENCES likes (id) ON DELETE SET NULL,
+  FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
+);
 
 CREATE TABLE tags (
-  id VARCHAR(50) PRIMARY KEY,
+  id SERIAL PRIMARY KEY,
   content text NOT NULL,
   createdAt TIMESTAMPTZ DEFAULT NOW(),
-  updatedAt TIMESTAMPTZ DEFAULT NOW(),
-)
+  updatedAt TIMESTAMPTZ DEFAULT NOW()
+);
 
 CREATE TABLE producttags (
-  product_id VARCHAR(50) NOT NULL,
-  tag_id VARCHAR(50) NOT NULL,
+  product_id INTEGER NOT NULL,
+  tag_id INTEGER NOT NULL,
   PRIMARY KEY (product_id, tag_id),
-  FOREIGN KEY (product_id) REFERENCES products (id),
-  FOREIGN KEY (tag_id) REFERENCES tags (id),
-)
+  FOREIGN KEY (product_id) REFERENCES products (id) ON DELETE SET NULL,
+  FOREIGN KEY (tag_id) REFERENCES tags (id) ON DELETE CASCADE
+);
 
 CREATE TABLE articletags (
-  article_id VARCHAR(50) NOT NULL,
-  tag_id VARCHAR(50) NOT NULL,
+  article_id INTEGER NOT NULL,
+  tag_id INTEGER NOT NULL,
   PRIMARY KEY (article_id, tag_id),
-  FOREIGN KEY (article_id) REFERENCES articles (id),
-  FOREIGN KEY (tag_id) REFERENCES tags (id),
-)
+  FOREIGN KEY (article_id) REFERENCES articles (id) ON DELETE SET NULL,
+  FOREIGN KEY (tag_id) REFERENCES tags (id) ON DELETE CASCADE
+);
 
 CREATE TABLE reservations (
-  id VARCHAR(50) PRIMARY KEY,
+  id SERIAL PRIMARY KEY,
+  user_id INTEGER NOT NULL,
+  product_id INTEGER NOT NULL,
   createdAt TIMESTAMPTZ DEFAULT NOW(),
   updatedAt TIMESTAMPTZ DEFAULT NOW(),
-  FOREIGN KEY user_id REFERENCES users (id), 
-  FOREIGN KEY product_id REFERENCES products (id),
-)
+  FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE,
+  FOREIGN KEY (product_id) REFERENCES products (id) ON DELETE CASCADE
+);
 
 COMMIT;
