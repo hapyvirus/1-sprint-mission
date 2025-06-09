900d7ce052ddcb3a912965aa9393dc71dbc87b95
hapyvirus <serenity2011@naver.com>
2025-06-09 16:10:18 +0900

    refactor: DEVELOPMENT => LOCAL

--- sprint_3/src/server.ts
+++ sprint_3/src/server.ts
@@ -2,6 +2,6 @@ import { server } from "./app";
 import { PORT } from "./lib/constants";
 
 server.listen(PORT, () => {
-  const env = process.env.NODE_ENV === "production" ? "PRODUCTION" : "DEVELOPMENT";
+  const env = process.env.NODE_ENV === "production" ? "PRODUCTION" : "LOCAL";
   console.log(`Server is listening on ${PORT} - Running in ${env} mode`);
 });
